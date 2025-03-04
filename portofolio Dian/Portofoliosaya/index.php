4bc313e2e0b2b6a369cf10c57e9dcd235af33035
DianHakim <ffmolen803@gmail.com>
2025-03-04 14:46:06 +0700

    Add files via upload

---
+++ portofolio Dian/Portofoliosaya/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+include 'layout/head.php';
+include 'layout/navbar.php';
+include 'layout/header.php';
+?>
+
+<body class="layout-fixed sidebar-expand-lg bg-body-tertiary">
+  <div class="app-wrapper">
+    <?php
+    include 'layout/section.php';
+    ?>
+
+    <?php
+    include 'layout/footer.php';
+    ?>
+  </div>
+  <?php
+  include 'layout/script.php';
+  ?>
+</body>
+
+</html>
---
+++ portofolio Dian/Portofoliosaya/layout/header.php
@@ -0,0 +1,10 @@
+    <!-- page header -->
+    <header id="home" class="header">
+        <div class="overlay"></div>
+        <div class="header-content container">
+            <h1 class="header-title">
+                <span class="up">HI!</span>
+                <span class="down">Saya Dian Hakim</span>
+            </h1>           
+        </div>              
+    </header><!-- end of page header -->
