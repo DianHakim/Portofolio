d23fee6462bd4c22afe13bc17bc238485e15a95c
DianHakim <ffmolen803@gmail.com>
2025-08-26 22:44:52 +0700

    Delete index.html directory

--- portofolio Dian/Portofoliosaya/layout/script.php
+++
@@ -1,9 +0,0 @@
-	<!-- core  -->
-    <script src="public_html/assets/vendors/jquery/jquery-3.4.1.js"></script>
-    <script src="public_html/assets/vendors/bootstrap/bootstrap.bundle.js"></script>
-
-    <!-- bootstrap 3 affix -->
-	<script src="public_html/assets/vendors/bootstrap/bootstrap.affix.js"></script>
-
-    <!-- Meyawo js -->
-    <script src="public_html/assets/js/meyawo.js"></script>
